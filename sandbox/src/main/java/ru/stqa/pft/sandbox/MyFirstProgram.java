4661eaf8be42c16c5e24171fe13a0508b764e08d
GriffinHeart4 <griffinheart4@gmail.com>
2018-03-27 13:59:33 +0300

    few functions are added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,7 +4,29 @@ public class MyFirstProgram
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello, world!");
+		hello("where is my bottle of rum?");
+
+		double l = 5;
+		System.out.println("square's area with side " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("rectangle's area with side " + a + " and " + b + " = " + area(a, b));
+	}
+
+	public static void hello(String somebody)
+	{
+		System.out.println("Hello, " + somebody);
+	}
+
+	public static double area(double l)
+	{
+		return l * l;
+	}
+
+	public static double area(double a, double b)
+	{
+		return a * b;
 	}
 }
 
