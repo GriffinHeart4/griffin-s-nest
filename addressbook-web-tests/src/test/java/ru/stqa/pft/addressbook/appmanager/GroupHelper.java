3ef3a7087af7f667e108c686988f2335ad709ab5
GriffinHeart4 <griffinheart4@gmail.com>
2018-03-29 18:55:36 +0300

    GroupModificationTests is created

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -45,4 +45,14 @@ public class GroupHelper extends HelperBase
             click(By.name("selected[]"));
         }
     }
+
+    public void initGroupModification()
+    {
+        click(By.name("edit"));
+    }
+
+    public void submitGroupModification()
+    {
+        click(By.name("update"));
+    }
 }
