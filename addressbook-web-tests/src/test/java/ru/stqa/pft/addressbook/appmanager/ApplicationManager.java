36a58be1a031abf3d74eadc42f35a505b6ed7265
GriffinHeart4 <griffinheart4@gmail.com>
2018-03-30 21:59:10 +0300

    Transitions between pages are optimized

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,7 +2,6 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -8,17 +8,27 @@ public class NavigationHelper extends HelperBase
 {
     public NavigationHelper(WebDriver wd)
     {
-
         super(wd);
     }
 
     public void gotoGroupPage()
     {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new")))
+        {
+            return;
+        }
         click(By.linkText("groups"));
+
     }
 
     public void gotoHomePage()
     {
+        if (isElementPresent(By.id("maintable")))
+        {
+            return;
+        }
         click(By.linkText("home"));
     }
 
