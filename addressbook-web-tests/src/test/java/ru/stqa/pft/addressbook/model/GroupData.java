90f2ceb31a478c32adbe77a2ae67b50bbbf7cc79
GriffinHeart4 <griffinheart4@gmail.com>
2018-04-04 22:26:11 +0300

    GroupCreationTests, GroupModificationTests are changed

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -22,7 +22,7 @@ public class GroupData
         this.name = name;
         this.header = header;
         this.footer = footer;
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
     }
 
     public String getName()
@@ -51,28 +51,27 @@ public class GroupData
     }
 
     @Override
-    public boolean equals(Object o)
-    {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GroupData groupData = (GroupData) o;
-        return id == groupData.id &&
-                Objects.equals(name, groupData.name);
-    }
-
-    @Override
-    public int hashCode()
-    {
-
-        return Objects.hash(id, name);
-    }
-
-    @Override
     public String toString()
     {
         return "GroupData{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode()
+    {
+
+        return Objects.hash(name);
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -20,9 +20,11 @@ public class GroupCreationTests extends TestBase
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
-
         group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
-        Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 }
